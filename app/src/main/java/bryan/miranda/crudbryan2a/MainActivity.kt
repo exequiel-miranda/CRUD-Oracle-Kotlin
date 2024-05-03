a299ee0146faf3042776fb12ad224852474e919d
Bryan <Bryan_miranda@ricaldone.edu.sv>
2024-05-03 11:01:16 -0600

    initial commit

---
+++ app/src/main/java/bryan/miranda/crudbryan2a/MainActivity.kt
@@ -0,0 +1,50 @@
+package bryan.miranda.crudbryan2a
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import modelo.ClaseConexion
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //1- Mandar a llamar todos los elementos de la pantalla
+        val txtNombre = findViewById<EditText>(R.id.txtNombre)
+        val txtPrecio = findViewById<EditText>(R.id.txtPrecio)
+        val txtCantidad = findViewById<EditText>(R.id.txtPrecio)
+        val btnAgregar = findViewById<Button>(R.id.btnAgregar)
+
+        //2- Programar el boton
+        btnAgregar.setOnClickListener {
+            GlobalScope.launch(Dispatchers.IO){
+
+                //Guardar datos
+                //1- Creo un objeto de la clase conexion
+                val claseC = ClaseConexion().cadenaConexion()
+
+                //2- creo una variable que contenga un PrepareStatement
+                val addProducto = claseC?.prepareStatement("insert into tbProducto(nombreProducto, precio, cantidad) values(?, ?, ?)")!!
+                addProducto.setString(1, txtNombre.text.toString())
+                addProducto.setInt(2, txtPrecio.text.toString().toInt())
+                addProducto.setInt(3, txtCantidad.text.toString().toInt())
+                addProducto.executeUpdate()
+            }
+        }
+
+    }
+}
